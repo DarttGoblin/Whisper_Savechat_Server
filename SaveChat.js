97765e8db02a5b1cb42bac1d75e589bccfccc1f6
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-08 15:06:57 +0100

    Update SaveChat.js

--- SaveChat.js
+++ SaveChat.js
@@ -14,39 +14,40 @@ app.use((req, res, next) => {
     next();
 });
 
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7724126',
+    password: 'V6PCDXyNdv',
+    database: 'sql7724126'
+});
+
 app.post("/", (req, res) => {
     const messageObj = req.body.messageObj;
 
     console.log(messageObj.sender);
     console.log(messageObj.message);
     
-    const connection = mysql.createConnection({
-        host: 'sql7.freesqldatabase.com',
-        user: 'sql7724126',
-        password: 'V6PCDXyNdv',
-        database: 'sql7724126'
-    });
-
-    connection.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error connecting to database: ' + err.stack);
-            res.status(500).json({ success: false, error: 'Error connecting to the database, ' + err.stack});
+            console.error('Error getting database connection: ' + err.stack);
+            res.status(500).json({ success: false, error: 'Error connecting to the database, ' + err.stack });
             return;
         }
         console.log('Connected to database with connection id ' + connection.threadId);
 
-        insertQuery = 'INSERT INTO conversation0(messageSender, messageContent) VALUES(?, ?)';
+        const insertQuery = 'INSERT INTO conversation0(messageSender, messageContent) VALUES(?, ?)';
         connection.query(insertQuery, [messageObj.sender, messageObj.message], (error) => {
+            connection.release();
+            
             if (error) {
                 console.log("Message hasn't been saved");
-                console.log('Error ' + error.stack);
+                console.error('Error ' + error.stack);
                 res.status(500).json({ success: false, error: error.stack });
                 return;
             }
-            else {
-                console.log('Message has been saved!');
-                res.status(200).json({ success: true});
-            }
+            console.log('Message has been saved!');
+            res.status(200).json({ success: true });
         });
     });
 });
